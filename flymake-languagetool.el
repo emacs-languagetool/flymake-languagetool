cc98ed4abc7ff9dd833e8bfd2552577ec5a2112f
Trey Peacock <git@treypeacock.com>
2022-11-14 09:30:26 -0700

    refactoring & formatting

--- flymake-languagetool.el
+++ flymake-languagetool.el
@@ -110,9 +110,6 @@ If non-nil, this list of strings replaces the standard java cli command."
                    "https://languagetool.org/http-api/swagger-ui/#!/default/post_check")
   :group 'flymake-languagetool)
 
-(defvar flymake-languagetool--started-server nil
-  "Have we ever attempted to start the LanguageTool server?")
-
 (defcustom flymake-languagetool-spelling-rules
   '("HUNSPELL_RULE"
     "HUNSPELL_RULE_AR"
@@ -202,7 +199,8 @@ STATUS provided from `url-retrieve'."
   (goto-char url-http-end-of-headers)
   (if (equal report-fn flymake-languagetool--report-fnc)
       (let* ((output (buffer-substring (point) (point-max)))
-             (errors (flymake-languagetool--output-to-errors output source-buffer))
+             (errors (flymake-languagetool--output-to-errors
+                      output source-buffer))
              (region (with-current-buffer source-buffer
                        (cons (point-min) (point-max)))))
         (save-restriction
@@ -211,32 +209,8 @@ STATUS provided from `url-retrieve'."
           (setq flymake-languagetool--report-fnc nil)))
     (flymake-log :warning "Canceling obsolete check %s" source-buffer)))
 
-(defun flymake-languagetool--start-server ()
-  "Start the LanguageTool server if we didn’t already."
-  (unless (process-live-p (get-process "languagetool-server"))
-    (let* ((cmd (or flymake-languagetool-server-command
-                    (list "java" "-cp" flymake-languagetool-server-jar
-                          "org.languagetool.server.HTTPServer"
-                          "--port" flymake-languagetool-server-port)))
-           (process (apply #'start-process "languagetool-server"
-                           " *LanguageTool server*"
-                           (append cmd flymake-languagetool-server-args))))
-      (set-process-query-on-exit-flag process nil)
-      (while
-          (with-current-buffer (process-buffer process)
-            (goto-char (point-min))
-            (unless (re-search-forward " Server started$" nil t)
-              (accept-process-output process 1)
-              (process-live-p process)))))))
-
-;;; Flymake
-(defun flymake-languagetool--start ()
+(defun flymake-languagetool--check ()
   "Run LanguageTool on the current buffer's contents."
-  (when (or flymake-languagetool-server-command
-            flymake-languagetool-server-jar)
-    (unless flymake-languagetool--started-server
-      (setq flymake-languagetool--started-server t)
-      (flymake-languagetool--start-server)))
   (let* ((report-fn flymake-languagetool--report-fnc)
          (url-request-method "POST")
          (url-request-extra-headers
@@ -262,6 +236,29 @@ STATUS provided from `url-retrieve'."
      #'flymake-languagetool--handle-finished
      (list source-buffer report-fn) t)))
 
+(defun flymake-languagetool--start ()
+  "Start the LanguageTool server if we didn’t already."
+  (if (or (not (or flymake-languagetool-server-command
+                   flymake-languagetool-server-jar))
+          (process-live-p (get-process "languagetool-server")))
+      (flymake-languagetool--check)
+    (let* ((source (current-buffer))
+           (cmd (or flymake-languagetool-server-command
+                    (list "java" "-cp" flymake-languagetool-server-jar
+                          "org.languagetool.server.HTTPServer"
+                          "--port" flymake-languagetool-server-port))))
+      (and (get-buffer " *LanguageTool server*")
+           (kill-buffer " *LanguageTool server*"))
+      (make-process
+       :name "languagetool-server" :noquery t :connection-type 'pipe
+       :buffer " *LanguageTool server*"
+       :command (append cmd flymake-languagetool-server-args)
+       :filter
+       (lambda (proc string)
+         (funcall #'internal-default-process-filter proc string)
+         (when (string-match ".*Server started\n$" string)
+           (with-current-buffer source (flymake-languagetool--check))
+           (set-process-filter proc nil)))))))
 
 (defun flymake-languagetool--checker (report-fn &rest _args)
   "Diagnostic checker function with REPORT-FN."
@@ -269,7 +266,7 @@ STATUS provided from `url-retrieve'."
   (setq flymake-languagetool--source-buffer (current-buffer))
   (flymake-languagetool--start))
 
-(defun flymake-languagetool--ovs ()
+(defun flymake-languagetool--ovs (&optional format)
   "List of all `flymake-languagetool' diagnostic overlays."
   (let* ((n 1)
          (ovs (flymake--overlays
@@ -279,29 +276,37 @@ STATUS provided from `url-retrieve'."
                                'flymake-languagetool--checker)))
                :compare (if (cl-plusp n) #'< #'>)
                :key #'overlay-start)))
-    ovs))
+    (if format
+        (seq-map
+         (lambda (ov) (cons (format "%s: %s"
+                                    (line-number-at-pos (overlay-start ov))
+                                    (flymake-diagnostic-text
+                                     (overlay-get ov 'flymake-diagnostic)))
+                            ov))
+         ovs)
+      ovs)))
 
 (defvar-local flymake-languagetool-current-cand nil
   "Current overlay candidate.")
 
-(defun flymake-languagetool--ov-at-point (&optional start)
+(defun flymake-languagetool--ov-at-point ()
   "Return `flymake-languagetool' overlay at point."
-  (catch 'overlay
-    (dolist (ol (overlays-at (or start (point))))
-      (when-let ((diag (overlay-get ol 'flymake-diagnostic)))
-        (when (eq (flymake-diagnostic-backend diag)
-                  'flymake-languagetool--checker)
-          (setq flymake-languagetool-current-cand ol)
-          (throw 'overlay ol))))))
+  (setq flymake-languagetool-current-cand
+        (car (flymake--overlays
+              :beg (point)
+              :filter
+              (lambda (ov)
+                (let ((diag (overlay-get ov 'flymake-diagnostic)))
+                  (eq (flymake-diagnostic-backend diag)
+                      'flymake-languagetool--checker)))))))
 
 (defun flymake-languagetool--suggestions ()
   "Show corrections suggested from LanguageTool."
-  (let ((ol flymake-languagetool-current-cand))
-    (overlay-put ol 'face 'isearch)
-    (map-elt
-     (flymake-diagnostic-data
-      (overlay-get ol 'flymake-diagnostic))
-     'suggestions)))
+  (overlay-put flymake-languagetool-current-cand 'face 'isearch)
+  (map-elt
+   (flymake-diagnostic-data
+    (overlay-get flymake-languagetool-current-cand 'flymake-diagnostic))
+   'suggestions))
 
 (defun flymake-languagetool--clean-overlay ()
   "Remove highlighting of current candidate."
@@ -318,10 +323,8 @@ STATUS provided from `url-retrieve'."
   (let* ((ovs (flymake-languagetool--ovs))
          (tail (cl-member-if (lambda (ov)
                                (if (cl-plusp n)
-                                   (> (overlay-start ov)
-                                      (point))
-                                 (< (overlay-start ov)
-                                    (point))))
+                                   (> (overlay-start ov) (point))
+                                 (< (overlay-start ov) (point))))
                              ovs))
          (chain (if flymake-wrap-around
                     (if tail
@@ -342,35 +345,29 @@ STATUS provided from `url-retrieve'."
   "Correct `flymake-languagetool' diagnostic at point.
 Use OL as diagnostic if non-nil."
   (interactive)
-  (unless (or ol (flymake-languagetool--ov-at-point))
-    (user-error "No correction at point"))
-  (when ol
-    (setq flymake-languagetool-current-cand ol))
-  (condition-case nil
-      (when-let ((ov flymake-languagetool-current-cand)
-                 (start (overlay-start ov))
-                 (end (overlay-end ov))
-                 (sugs (flymake-languagetool--suggestions))
-                 (prompt (flymake-diagnostic-text (overlay-get ov 'flymake-diagnostic)))
-                 (choice (condition-case nil
-                             (completing-read (format "Correction (%s): " prompt) sugs)
-                           (t (flymake-languagetool--clean-overlay)))))
-        (delete-region start end)
-        (goto-char start)
-        (insert choice)
-        (setq flymake-languagetool-current-cand nil))
-    (t (flymake-languagetool--clean-overlay))))
+  (if-let (flymake-languagetool-current-cand
+           (or ol (flymake-languagetool--ov-at-point)))
+      (condition-case nil
+          (when-let ((ov flymake-languagetool-current-cand)
+                     (start (overlay-start ov))
+                     (end (overlay-end ov))
+                     (sugs (flymake-languagetool--suggestions))
+                     (prompt (flymake-diagnostic-text
+                              (overlay-get ov 'flymake-diagnostic)))
+                     (choice (completing-read
+                              (format "Correction (%s): " prompt) sugs)))
+            (delete-region start end)
+            (goto-char start)
+            (insert choice)
+            (setq flymake-languagetool-current-cand nil))
+        (t (flymake-languagetool--clean-overlay)))
+    (user-error "No correction at point")))
 
 ;;;###autoload
 (defun flymake-languagetool-correct ()
   "Use `completing-read' to select and correct diagnostic."
   (interactive)
-  (let* ((ovs (flymake-languagetool--ovs))
-         (cands (seq-map (lambda (ov)
-                           (cons (format "%s: %s" (line-number-at-pos (overlay-start ov))
-                                         (flymake-diagnostic-text (overlay-get ov 'flymake-diagnostic)))
-                                 ov))
-                         ovs))
+  (let* ((cands (flymake-languagetool--ovs 'format))
          (cand (if cands
                    (completing-read "Error: " cands)
                  (user-error "No candidates")))
