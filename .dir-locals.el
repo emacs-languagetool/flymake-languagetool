c70f7d716ab1ea6726c0d57abcf4468bea90a3e3
JenChieh <jcs090218@gmail.com>
2024-03-02 15:04:25 -0800

    style: Add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((markdown-mode   . ((fill-column      . 80)))
+ (emacs-lisp-mode . ((fill-column      . 80)
+                     (indent-tabs-mode . nil))))
